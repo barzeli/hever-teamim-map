df1e0c7ccb9578f0c958b1227d86310483d9ca26
barzeli <evya7777777@gmail.com>
2024-10-03 17:00:43 +0300

    used top level await

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,61 @@
-import { XmlElement, toXML } from "jstoxml";
-import { APIResponse } from "./types";
+import jsontoxml from "jstoxml";
 import { writeFile } from "fs/promises";
 
-const run = async () => {
-  const response: APIResponse = await (
-    await fetch("https://www.hvr.co.il/bs2/datasets/teamimcard_branches.json")
-  ).json();
+export interface PlaceResponse {
+  img: string;
+  name: string;
+  desc: string;
+  area: string;
+  city: string;
+  address: string;
+  phone: string;
+  category: string;
+  type: string;
+  hours: string;
+  kosher: string;
+  handicap: string;
+  website: string;
+  delivery: string;
+  internal_link: string;
+  is_new: string;
+  search_words: string;
+  latitude: string;
+  longitude: string;
+}
 
-  const kmlObject: XmlElement = {
-    kml: {
-      _attrs: {
-        xmlns: "http://www.opengis.net/kml/2.2",
-      },
-      Document: [
-        { name: "חבר טעמים" },
-        ...response.branch.map((place) => ({
-          Placemark: {
-            name: place.name,
-            description: `${place.desc}
+export interface APIResponse {
+  branch: PlaceResponse[];
+}
+
+const response: APIResponse = await (
+  await fetch("https://www.hvr.co.il/bs2/datasets/teamimcard_branches.json")
+).json();
+
+const kmlObject: jsontoxml.XmlElement = {
+  kml: {
+    _attrs: {
+      xmlns: "http://www.opengis.net/kml/2.2",
+    },
+    Document: [
+      { name: "חבר טעמים" },
+      ...response.branch.map((place) => ({
+        Placemark: {
+          name: place.name,
+          description: `${place.desc}
 ${place.category} - ${place.type}
 ${place.hours}
 ${place.address}, ${place.city}`,
-            Point: {
-              coordinates: `${place.longitude},${place.latitude},0`,
-            },
+          Point: {
+            coordinates: `${place.longitude},${place.latitude},0`,
           },
-        })),
-      ],
-    },
-  };
-
-  const xml = toXML(kmlObject, {
-    header: true,
-    indent: "  ",
-  });
-  await writeFile("./places.kml", xml);
+        },
+      })),
+    ],
+  },
 };
 
-run();
+const xml = jsontoxml.toXML(kmlObject, {
+  header: true,
+  indent: "  ",
+});
+await writeFile("./places.kml", xml);
